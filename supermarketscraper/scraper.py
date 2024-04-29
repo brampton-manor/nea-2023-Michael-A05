634ec6555ac35a14bfd74adda44b5fef95448db1
Michael-A05 <abayojumumichael@gmail.com>
2024-04-29 09:04:05 +0100

    improved webdriver efficiency: ensuring that processes are terminated and browsers are closed

--- supermarketscraper/scraper.py
+++ supermarketscraper/scraper.py
@@ -1,5 +1,6 @@
 import logging
 import time
+import psutil
 
 import requests
 from selenium import webdriver
@@ -17,7 +18,7 @@ class Scraper:
         self.supermarkets = supermarkets
         self.database = database
 
-    def scrape(self):
+    def scrape(self) -> None:
         try:
             return self.scrape_cycle()
         except requests.exceptions.Timeout:
@@ -55,8 +56,6 @@ class Scraper:
                                      "Frozen", "fresh", "frozen"]
 
                 if category_name in wanted_categories:
-                    if category_name == "bakery" and supermarket.name == "Aldi":
-                        continue
 
                     log.info(f"Retrieving {category_name} products")
 
@@ -66,9 +65,10 @@ class Scraper:
                     finished_category = False
 
                     while not finished_category:
+                        empty_string = ""
                         url = supermarket.build_url(url=start_page_url, page=page)
 
-                        if not url:
+                        if url == empty_string:
                             # Get the category products if they are shown on a single page e.g. Morrisons
                             html = self.get_html(url=start_page_url)
                             supermarket_category_products = supermarket.filter_products(html)
@@ -136,52 +136,47 @@ class Scraper:
                                 log.warning(f"{product.product_name} has no nutritional information")
                                 continue
 
-    def get_html(self, url):
-        log.info(f"Scraping {url}")
+    def setup_driver(self):
         user_agent = ("Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) "
                       "Mobile/15E148")
 
         options = Options()
         options.add_argument("start-maximized")
-        # options.add_argument('--headless')
-        options.add_argument("--disable-site-isolation-trials")
         options.add_argument("--disable-gpu")
-        options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        options.add_experimental_option("excludeSwitches", ["enable-logging"])
+        options.add_experimental_option("excludeSwitches", ["enable-automation", "enable-logging"])
         options.add_experimental_option("useAutomationExtension", False)
         options.add_argument("--disable-blink-features=AutomationControlled")
         options.add_argument("--no-sandbox")
         options.add_argument(f"user-agent={user_agent}")
 
+        return webdriver.Chrome(options=options)
+
+    def get_html(self, url: str) -> str | None:
+        log.info(f"Scraping {url}")
+        driver = None
         try:
-            driver = webdriver.Chrome(options=options)
+            driver = self.setup_driver()
             driver.get(url)
+            time.sleep(5)
+            html = driver.page_source
+            return html
         except (WebDriverException, Exception) as e:
-            log.error(f"Selenium exception: {e.msg}")
+            log.error(f"Selenium exception: {e}")
             return None
+        finally:
+            if driver:
+                driver_process = psutil.Process(driver.service.process.pid)
+                if driver_process.is_running():
+                    driver.close()
+                    driver_process.terminate()
+                else:
+                    log.info("Driver has quit successfully")
 
-        time.sleep(5)
-        html = driver.page_source
-        driver.quit()
-        return html
-
-    def get_page(self, url):
+    def get_page(self, url: str) -> str | None:
         log.info(f"Scraping {url}")
-        user_agent = ("Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) "
-                      "Mobile/15E148")
-
-        options = Options()
-        options.add_argument("start-maximized")
-        options.add_argument("--disable-gpu")
-        options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        options.add_experimental_option("excludeSwitches", ["enable-logging"])
-        options.add_experimental_option("useAutomationExtension", False)
-        options.add_argument("--disable-blink-features=AutomationControlled")
-        options.add_argument("--no-sandbox")
-        options.add_argument(f"user-agent={user_agent}")
-
+        driver = None
         try:
-            driver = webdriver.Chrome(options=options)
+            driver = self.setup_driver()
             driver.get(url)
             wait = WebDriverWait(driver, 10)
             element = wait.until(
@@ -189,7 +184,6 @@ class Scraper:
             )
             time.sleep(5)
             html = driver.page_source
-            driver.quit()
             return html
         except TimeoutException:
             log.warning(f"Time out waiting for an element")
@@ -197,3 +191,11 @@ class Scraper:
         except Exception as e:
             log.error(f"An error occurred while fetching the page: {e}")
             return None
+        finally:
+            if driver:
+                driver_process = psutil.Process(driver.service.process.pid)
+                if driver_process.is_running():
+                    driver.close()
+                    driver_process.terminate()
+                else:
+                    log.info("Driver has quit successfully")
